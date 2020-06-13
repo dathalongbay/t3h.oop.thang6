d32ac2cfb33c18d2fc69ec8573115ad92245e293
dat <dathalongbay@gmail.com>
2020-06-13 18:49:49 +0700

    1

--- sourcecodes/001/001.php
+++ sourcecodes/001/001.php
@@ -27,7 +27,12 @@ class Hinhanh {
     // sử dụng từ khóa public khai báo giới hạn truy cập trước tên của phương thức
     // phương thức trong class thường dùng để tạo 1 hành động cho đối tượng
     public function html() {
-        $html = "<img src='' alt='' width='' height='' id='' class='' />";
+
+        // đưa các thuộc tính của class HinhAnh vào trong phương thức html
+        // để truy cập vào thuộc tính trong class ở trong nội bộ
+        // phương thức của nó thì sử dụng $this->ten_thuoc_tinh
+        // $this->src
+        $html = "<img src='$this->src' alt='$this->alt' width='$this->width' height='$this->height' id='$this->id' class='$this->class' />";
 
         return $html;
     }
